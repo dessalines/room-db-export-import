3c1728d3f765476075ea76b43b780fa74287ecc4
Dessalines <tyhou13@gmx.com>
2024-09-29 15:40:53 -0400

    Initial commit.

---
+++ room-db-export-import/src/main/java/com/room_db_export_import/test.kt
@@ -0,0 +1,5 @@
+package com.room_db_export_import
+
+fun test(): Int {
+    return 2+3
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "room-db-export-import"
+include(":app")
+include(":room-db-export-import")
