216bd208168737935a9fab5dd963027867ae4b5d
Dessalines <tyhou13@gmx.com>
2026-05-15 22:03:28 -0400

    Fixing compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.roomdbexportimportapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.roomdbexportimportapp"
